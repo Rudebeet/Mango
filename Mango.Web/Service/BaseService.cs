306b64a63cc3186e724ebe3b46a9269d2b23c90c
Sridhar K <sridhar@Sridhars-MacBook-Air.local>
2024-03-19 01:53:45 +0530

    Web App

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Newtonsoft.Json;
+using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Service
+{
+	public class BaseService : IBaseService
+	{
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;       
+        }
+
+
+        public HttpMethod Method { get; private set; }
+
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            try
+            {
+                HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+                HttpRequestMessage message = new();
+
+                message.Headers.Add("Accept", "application/json");
+                //token
+
+                message.RequestUri = new Uri(requestDto.Url);
+
+                if (requestDto.Data is not null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage? apiReponse = new();
+
+                switch (requestDto.ApiVerb)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiReponse = await client.SendAsync(message);
+
+                switch (apiReponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                    case HttpStatusCode.Forbidden:
+                        return new() { IsSuccess = false, Message = "Access Denied" };
+                    case HttpStatusCode.Unauthorized:
+                        return new() { IsSuccess = false, Message = "Unauthorized" };
+                    case HttpStatusCode.InternalServerError:
+                        return new() { IsSuccess = false, Message = "Internal Server Error" };
+                    default:
+                        var apiContent = await apiReponse.Content.ReadAsStringAsync();
+                        var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                        return apiResponseDto;
+                }
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDto()
+                {
+                    IsSuccess = false,
+                    Message = ex.Message.ToString()
+                };
+            return dto;
+            }
+            
+        }
+    }
+}
+
---
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+	public interface ICouponService
+	{
+		Task<ResponseDto?> GetCouponAsync(String couponCode);
+        Task<ResponseDto?> GetCouponByIdAsync(int id);
+        Task<ResponseDto?> GetAllCouponsAsync();
+        Task<ResponseDto?> CreateNewCouponsync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateCouponsync(int id, CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponAsync(int id);
+
+    }
+}
+
+
